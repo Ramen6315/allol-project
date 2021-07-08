c6960281791c03734be6f6fcb610f2519b914622
ramen6315 <ramen6315@gmail.com>
2021-07-09 01:15:59 +0900

    feat: riot api feign 설정

--- src/main/java/anylol/all/about/lol/riot/RiotApi.java
+++ src/main/java/anylol/all/about/lol/riot/RiotApi.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value = "riotApi", url = "https://kr.api.riotgames.com", configuration = {FeignConfiguration.class})
 public interface RiotApi {
 
-    @GetMapping(value = "/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=RGAPI-9d807a64-d3b0-4ca7-a697-b44d46cfe51d")
+    @GetMapping(value = "/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=")
     Summoner getSummoner(@PathVariable("summonerName") String summonerName);
 }
