877666735692ea4baca8db1f766d19615902b059
ramen6315 <ramen6315@gmail.com>
2021-08-26 21:42:31 +0900

    delete

--- src/main/java/anylol/all/about/lol/riot/RiotApi.java
+++ src/main/java/anylol/all/about/lol/riot/RiotApi.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value = "riotApi", url = "https://kr.api.riotgames.com", configuration = {FeignConfiguration.class})
 public interface RiotApi {
 
-    @GetMapping(value = "/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=RGAPI-089cd6a9-0813-4ee7-9025-f2708484dc45")
+    @GetMapping(value = "/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=")
     Summoner getSummoner(@PathVariable("summonerName") String summonerName);
 
-    @GetMapping(value = "/lol/match/v4/matchlists/by-account/{encryptedAccountId}?api_key=RGAPI-089cd6a9-0813-4ee7-9025-f2708484dc45")
+    @GetMapping(value = "/lol/match/v4/matchlists/by-account/{encryptedAccountId}?api_key=")
     MatchList getMatches(@PathVariable("encryptedAccountId") String encryptedAccountId);
 
-    @GetMapping(value = "/lol/platform/v3/champion-rotations?api_key=RGAPI-089cd6a9-0813-4ee7-9025-f2708484dc45")
+    @GetMapping(value = "/lol/platform/v3/champion-rotations?api_key=")
     RotationChampion getRotationChampion();
 }
